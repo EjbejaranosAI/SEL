c5c2f31b5d392d46c78d560bf77ff35e7d1f0e99
Ejbejaranos <91475734+EjbejaranosAI@users.noreply.github.com>
2022-04-11 01:10:21 +0200

    utils function created

--- 1.RISE/utils/utils.py
+++ 1.RISE/utils/utils.py
@@ -17,4 +17,19 @@ def name_datasets(path_datasets):
     print(f"You find {len(name_datasets)} datasets")
     return name_datasets
 
-name_datasets("../databases")
+def load_dataset(path,name_path,columns_name):
+    df = []
+    df = pd.read_csv(path+'/'+name_path)
+    df.columns = columns_name
+    df.head(5)
+
+    return df
+
+columns_name_wine = ['class','Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols','Proanthocyanins','Color intensity','Hue','OD280/OD315 of diluted wines','Proline']
+
+nm_data = name_datasets("../databases")
+df = load_dataset("../databases",nm_data[0],columns_name_wine)
+print(df.head(5))
+print(df.unique())
+
+
